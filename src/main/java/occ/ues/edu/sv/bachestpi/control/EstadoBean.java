76beaaeb11f8a9b1d637b0d06fdb65d544af32b4
dg17001 <dg17001@ues.edu.sv>
2022-03-04 14:43:40 -0600

    Metodo eliminar entidad estado

--- src/main/java/occ/ues/edu/sv/bachestpi/control/EstadoBean.java
+++ src/main/java/occ/ues/edu/sv/bachestpi/control/EstadoBean.java
@@ -3,8 +3,10 @@ package occ.ues.edu.sv.bachestpi.control;
 import java.io.Serializable;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import occ.ues.edu.sv.bachestpi.control.exceptions.NonexistentEntityException;
 import occ.ues.edu.sv.bachestpi.entity.Estado;
 
 public class EstadoBean implements Serializable{
@@ -27,8 +29,23 @@ public class EstadoBean implements Serializable{
         return false;
     }
 
-    public boolean eliminar(Estado eliminar){
+    public boolean eliminar(Integer id) throws NonexistentEntityException{
+        Estado eliminar;
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("bachesUP");
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction tx=em.getTransaction();
+        eliminar=em.getReference(Estado.class,id);
         
+        try {
+            tx.begin();
+            eliminar.getIdEstado();
+            em.remove(eliminar);
+            tx.commit();
+            return true;
+            
+        } catch (EntityNotFoundException enfe) {
+            tx.rollback();
+        }
         return false;
     }
 }
