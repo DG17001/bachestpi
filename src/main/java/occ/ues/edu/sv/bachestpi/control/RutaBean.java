ca932e218d0512451cd49e3b5041dce44f549df1
dg17001 <dg17001@ues.edu.sv>
2022-03-04 10:54:11 -0600

    test metodo crear entidad ruta

--- src/main/java/occ/ues/edu/sv/bachestpi/control/RutaBean.java
+++ src/main/java/occ/ues/edu/sv/bachestpi/control/RutaBean.java
@@ -1,34 +1,29 @@
 package occ.ues.edu.sv.bachestpi.control;
 
-import java.io.Serializable;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import occ.ues.edu.sv.bachestpi.entity.Estado;
 
-public class EstadoBean implements Serializable{
+public class RutaBean {
 
-    public boolean crear(Estado nuevo){
-
+    public boolean crear(Estado nuevo) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("bachesUP");
         EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
+        EntityTransaction et = em.getTransaction();
 
         try {
-            tx.begin();
+
+            et.begin();
             em.persist(nuevo);
-            tx.commit();
+            et.commit();
             return true;
-            
         } catch (Exception e) {
-            tx.rollback();
+            et.rollback();
         }
+
         return false;
     }
 
-    public boolean eliminar(Estado eliminar){
-        
-        return false;
-    }
 }
