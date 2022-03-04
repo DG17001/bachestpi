60982f43dd41c8c2a9faa40aa392aeb65305e3b7
dg17001 <dg17001@ues.edu.sv>
2022-03-04 14:43:58 -0600

    Test metodo eliminar entidad estado

--- src/test/java/occ/ues/edu/sv/bachestpi/control/EstadoBeanTest.java
+++ src/test/java/occ/ues/edu/sv/bachestpi/control/EstadoBeanTest.java
@@ -8,6 +8,7 @@ package occ.ues.edu.sv.bachestpi.control;
 import java.util.Date;
 import occ.ues.edu.sv.bachestpi.entity.Estado;
 import static org.junit.jupiter.api.Assertions.*;
+import sun.awt.X11.XConstants;
 
 /**
  *
@@ -20,16 +21,26 @@ public class EstadoBeanTest {
 
     /**
      * Test of crear method, of class EstadoBean.
-     */
+     
     @org.junit.jupiter.api.Test
     public void testCrear() {
-        System.out.println("crear");
+        System.out.println("Crear");
         Estado nuevo = new Estado();
         nuevo.setFechaCreacion(new Date());
         nuevo.setNombre("Creado desde EstadoBeanTest "+System.currentTimeMillis());
         EstadoBean instance = new EstadoBean();
         boolean expResult = true;
         boolean result = instance.crear(nuevo);
+        assertEquals(expResult, result);
+    }
+    */
+    @org.junit.jupiter.api.Test
+    public void testEliminar(){
+        System.out.println("Eliminar");
+        Estado delete= new Estado();
+        EstadoBean instance=new EstadoBean();
+        boolean expResult=true;
+        boolean result=instance.eliminar(12);
         assertEquals(expResult, result);
     }
     
