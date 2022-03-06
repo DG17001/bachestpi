3022e582a0ef0d13a5f5e11cbc45e9e4d3c0aff5
dg17001 <dg17001@ues.edu.sv>
2022-03-06 10:46:25 -0600

    test metodo modificar entidad objeto

--- src/test/java/occ/ues/edu/sv/bachestpi/control/ObjetoBeanTest.java
+++ src/test/java/occ/ues/edu/sv/bachestpi/control/ObjetoBeanTest.java
@@ -59,4 +59,5 @@ public class ObjetoBeanTest extends TestCase {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     
+   }
 }
