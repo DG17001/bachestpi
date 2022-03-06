04810d17c01605023ff567498bfa0a1a0d52e1b2
dg17001 <dg17001@ues.edu.sv>
2022-03-06 10:35:38 -0600

    metodo crear entidad objeto

---
+++ src/main/java/occ/ues/edu/sv/bachestpi/control/ObjetoBean.java
@@ -0,0 +1,36 @@
+package occ.ues.edu.sv.bachestpi.control;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import occ.ues.edu.sv.bachestpi.control.exceptions.NonexistentEntityException;
+import occ.ues.edu.sv.bachestpi.entity.Objeto;
+
+public class ObjetoBean {
+    
+    public boolean crear(Objeto nuevo){
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bachesUP");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            em.persist(nuevo);
+            tx.commit();
+            return true;
+            
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+           if (em!=null){
+            em.close();
+        }
+        }
+        return false;
+    }
+
+    
+    
+}
