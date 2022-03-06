37fa26b9e614c56f3eea8a87b97f151c8546f4a7
dg17001 <dg17001@ues.edu.sv>
2022-03-06 10:38:07 -0600

    test metodo eliminar entidad objeto

--- src/test/java/occ/ues/edu/sv/bachestpi/control/ObjetoBeanTest.java
+++ src/test/java/occ/ues/edu/sv/bachestpi/control/ObjetoBeanTest.java
@@ -31,6 +31,19 @@ public class ObjetoBeanTest extends TestCase {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
     
+    /**
+     * Test of eliminar method, of class ObjetoBean.
+    */ 
+    public void testEliminar() throws Exception {
+        System.out.println("eliminar");
+        Integer id = null;
+        ObjetoBean instance = new ObjetoBean();
+        boolean expResult = false;
+        boolean result = instance.eliminar(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+       
 }
