149199c37737b7d7cd684a2b06bbaac5a4888bc7
dg17001 <dg17001@ues.edu.sv>
2022-03-06 10:36:20 -0600

    test metodo crear entidad objeto

---
+++ src/test/java/occ/ues/edu/sv/bachestpi/control/ObjetoBeanTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package occ.ues.edu.sv.bachestpi.control;
+
+import junit.framework.TestCase;
+import occ.ues.edu.sv.bachestpi.entity.Objeto;
+
+/**
+ *
+ * @author magdiel
+ */
+public class ObjetoBeanTest extends TestCase {
+    
+    public ObjetoBeanTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Test of crear method, of class ObjetoBean.
+     */
+    public void testCrear() {
+        System.out.println("crear");
+        Objeto nuevo = null;
+        ObjetoBean instance = new ObjetoBean();
+        boolean expResult = false;
+        boolean result = instance.crear(nuevo);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    
+}
