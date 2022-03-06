55b94b02fac4852947475a2731c1ccb53ebb271c
dg17001 <dg17001@ues.edu.sv>
2022-03-06 15:16:14 -0600

    metodo crear entidad ObjetoEstado

---
+++ src/main/java/occ/ues/edu/sv/bachestpi/control/ObjetoEstadoBean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package occ.ues.edu.sv.bachestpi.control;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import occ.ues.edu.sv.bachestpi.entity.Objeto;
+
+/**
+ *
+ * @author magdiel
+ */
+public class ObjetoEstadoBean {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("bachesUP");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    
+    public boolean crear(Objeto nuevo){      
+        try {
+            tx.begin();
+            em.persist(nuevo);
+            tx.commit();
+            return true;
+            
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+           if (em!=null){
+            em.close();
+        }
+        }
+        return false;
+    }
+    
+}
