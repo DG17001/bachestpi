78cfca44965f188c43780a787bcd158e0319bcf3
dg17001 <dg17001@ues.edu.sv>
2022-03-06 11:12:22 -0600

    metodo eliminar entidad TipoObjeto

--- src/main/java/occ/ues/edu/sv/bachestpi/control/TipoObjetoBean.java
+++ src/main/java/occ/ues/edu/sv/bachestpi/control/TipoObjetoBean.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import occ.ues.edu.sv.bachestpi.control.exceptions.NonexistentEntityException;
 import occ.ues.edu.sv.bachestpi.entity.TipoObjeto;
 
 /**
@@ -37,5 +38,29 @@ public class TipoObjetoBean {
         }
         }
         return false;
-    }   
+    }
+ 
+    public boolean eliminar(Integer id) throws NonexistentEntityException{
+        TipoObjeto eliminar;
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("bachesUP");
+        EntityManager em=emf.createEntityManager();
+        EntityTransaction tx=em.getTransaction();
+        eliminar=em.getReference(TipoObjeto.class,id);
+        
+        try {
+            tx.begin();
+            eliminar.getIdTipoObjeto();
+            em.remove(eliminar);
+            tx.commit();
+            return true;
+            
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+           if (em!=null){
+            em.close();
+        }
+        }
+        return false;
+    }
 }
